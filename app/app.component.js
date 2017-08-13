4494301dd250ca367146baf876ba359b93fcf0f3
Justyna Foster <justynawfoster@gmail.com>
2017-08-13 12:55:04 -0600

    init commit

---
+++ app/app.component.js
@@ -0,0 +1,19 @@
+(function(){
+  'use strict';
+
+  angular.module("app")
+    .component('app', {
+      controller: controller,
+      templateUrl: 'app/app.template.html'
+    })
+
+    function controller(){
+      const vm = this;
+
+      vm.$onInit = onInit;
+
+      function onInit(){
+        console.log("you're in app component!");
+      }
+    }
+})();
